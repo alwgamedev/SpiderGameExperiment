6daf5db0a449813910ee86af6053c5a660960971
alwgamedev <yasserfangirl67@gmail.com>
2025-11-19 13:25:32 -0500

    renamed bones and fixed anim properties

---
+++ Assets/Scripts/Core/Editor/CopyPathMenuItem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+
+public static class CopyPathMenuItem
+{
+    [MenuItem("GameObject/Copy Path")]
+    private static void CopyPath()
+    {
+        var go = Selection.activeGameObject;
+
+        if (go == null)
+        {
+            return;
+        }
+
+        var path = go.name;
+
+        while (go.transform.parent != null)
+        {
+            go = go.transform.parent.gameObject;
+            path = $"{go.name}/{path}";
+        }
+
+        EditorGUIUtility.systemCopyBuffer = path;
+    }
+
+    [MenuItem("GameObject/Copy Path", true)]
+    private static bool CopyPathValidation()
+    {
+        //don't show copy path option when multiple game objects are selected
+        return Selection.gameObjects.Length == 1;
+    }
+}
