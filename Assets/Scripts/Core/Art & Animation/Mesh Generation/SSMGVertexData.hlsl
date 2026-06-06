bd7162883f395e499c7d859e2047d4d3cb86f064
alwgamedev <yasserfangirl67@gmail.com>
2026-06-06 13:08:51 -0400

    work on ssmg shader -- ht map looking decent
    need to work on boundary stuff

--- Assets/Scripts/Core/Art & Animation/Mesh Generation/SSMGVertexData.hlsl
+++ Assets/Scripts/Core/Art & Animation/Mesh Generation/SSMGVertexData.hlsl
@@ -26,6 +26,7 @@
         float4 jacInv = ScreenToWorldJacobian(worldPos);
         float hx = ddxWorld(height, jacInv);
         float hy = ddyWorld(height, jacInv);
+        float hxx = ddxWorld(hx, jacInv);
         nz = rsqrt(1 + hx * hx + hy * hy);
     }
 
